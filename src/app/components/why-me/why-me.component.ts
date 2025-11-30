93d8c720be97f58d2a5f6729dc1e471138931d49
AljassimAkram <akramwljasem1995@gmail.com>
2025-12-01 00:32:12 +0100

    update

---
+++ src/app/components/why-me/why-me.component.ts
@@ -0,0 +1,9 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-why-me',
+  templateUrl: './why-me.component.html',
+  styleUrls: ['./why-me.component.scss'],
+})
+export class WhyMeComponent {}
